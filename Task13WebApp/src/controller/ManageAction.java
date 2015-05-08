4c8e5326379c9133b53c3e6a12ab704a00d6e13a
charlottelinxue <charlottelinxue@gmail.com>
2015-05-08 11:44:58 -0400

    add prediction

--- Task13WebApp/src/controller/ManageAction.java
+++ Task13WebApp/src/controller/ManageAction.java
@@ -54,6 +54,45 @@ public class ManageAction extends Action {
 	}
 
 	public String perform(HttpServletRequest request) {
+		// get route
+		
+		// get direction
+		
+		// get all stops
+		
+		
+		/*********************/
+		// get route (if type in route id directly, then dont need database at all)
+		// below hard code one route name, to test
+		String routeNameToSearch = "FREEPORT ROAD";
+		// route id need to take from database according to route name.
+		String routeIdToUse = "1";
+		
+		// get stop (api need route id and direction, which we will input)
+		// below hard code one stop name, to test
+		String stopNameToSearch = "31st St Bridge Ramp at Rialto St";
+		// route id need to take from database according to stop name.
+		String stopIdToUse = "20781";
+		// size is 2, time[0] is time stmp, time[1] is predicted time
+		String time[];
+		try {
+			time = getTime(routeIdToUse, stopIdToUse);
+			String timeStamp = time[0];
+			String timePredicted = time[1];
+			System.out.println(timeStamp);
+			System.out.println(timePredicted);
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		
+		/*********************/
+		
+		
+		// get time
+		// use prediction api to get time.
+		
+		
 		
 		// Get all route name from database
 		// show on the page if no parameter passed in
@@ -67,7 +106,7 @@ public class ManageAction extends Action {
 		// no need code here
 		
 		// below are trying to use get all routes api
-		
+		/*
 		try {
 			getAllRoutes();
 			System.out.println("I get here!");
@@ -75,6 +114,9 @@ public class ManageAction extends Action {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		*/
+		// get all stops
+		
 		
 		
 		
@@ -111,7 +153,6 @@ public class ManageAction extends Action {
         
     }
 	private static void getAllRoutes() throws IOException {
-		System.out.println("I get here???");
 		HttpURLConnection connection = null;
 		URL url = new URL("http://truetime.portauthority.org/bustime/"
 				+ "api/v2/getroutes?key=ADpCvpyDcupACyuMdk5wrVTVH&format=json");
@@ -139,6 +180,40 @@ public class ManageAction extends Action {
 		System.out.println(route1Id);
 		String route1Name = (String) route1.get("rtnm");
 		System.out.println(route1Name);
+	}
+	
+	private static String[] getTime(String routeId, String stopId) throws IOException {
+		HttpURLConnection connection = null;
+		URL url = new URL("http://truetime.portauthority.org/bustime/"
+				+ "api/v2/getpredictions?key=ADpCvpyDcupACyuMdk5wrVTVH"
+				+ "&rt=" + routeId + "&stpid=" + stopId 
+				+ "&format=json");
+		System.out.println(url.toString());
+		connection =  (HttpURLConnection) url.openConnection();
+		connection.setRequestMethod("GET");
+		connection.setDoOutput(true);
+		connection.setDoInput(true);
+		connection.setUseCaches(false);
+				
+		JSONObject obj = (JSONObject) JSONValue
+				.parse(readResponse(connection));
+		JSONObject results = (JSONObject) obj.get("bustime-response");
+		System.out.println(results.toString());
+		JSONArray times = (JSONArray) results.get("prd");
+		System.out.println("Times is null: " + (times == null));
+//		System.out.println("results is null: " + (results == null || results.size() == 0));
+		
+//		JSONObject result = (JSONObject) results.get(0);
+//		JSONObject geometry = (JSONObject) result.get("geometry");
+//		System.out.println("geometry" + geometry.toString());
+//		JSONObject location = (JSONObject) geometry.get("location");
+//		System.out.println("location" + location.toString());
+		JSONObject time = (JSONObject) times.get(0);
+		String[] prediction = new String[2];
+		prediction[0] = (String) time.get("tmstmp");
+		prediction[1] = (String) time.get("prdtm");
+		System.out.println(Arrays.toString(prediction));
+		return prediction;
 	}
 	private static String readResponse(HttpURLConnection connection) {
 		try {
